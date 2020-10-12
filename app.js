27037792de93a1c63d787f43d44f91c94744b32c
jonathanSmartbear <56030954+jonathanSmartbear@users.noreply.github.com>
2020-10-12 15:23:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const app = express();
-const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
-
 const port = process.env.PORT || 5000;
+
+const swaggerJsDoc = require("swagger-jsdoc");
+
+
 
 // Extended: https://swagger.io/specification/#infoObject
 const swaggerOptions = {
   swaggerDefinition: {
     info: {
-      title: "Customer API",
-      description: "Customer API Information",
+      title: "Demo API",
+      description: "API Information",
       contact: {
-        name: "Amazing Developer"
+        name: "JonathanH"
       },
       servers: ["http://localhost:5000"]
     }
@@ -21,54 +22,39 @@ const swaggerOptions = {
   apis: ["app.js"]
 };
 
+
+
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-//console.log(swaggerDocs);
-///////////////////////////////////////////////////////////////////
+
 // prints the doc to /doc
 app.get("/doc",(req,res) =>{
   res.json(swaggerDocs);
 })
 
-// write to json file
-const fs = require('fs');
-
-fs.writeFile("apiSpec.json", JSON.stringify(swaggerDocs, null, 4), function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-
-    server.close(() => {
-      console.log(`Server closed`);
-    });
-}); 
-
-////////////////////////////////////////////////////////////////////
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
 // Routes
 /**
  * @swagger
- * /customers:
+ * /demo:
  *  get:
- *    description: Use to request all customers
+ *    description: Use to request list
  *    responses:
  *      '200':
  *        description: A successful response
  */
-app.get("/customers", (req, res) => {
-  res.status(200).send("Customer results");
+
+app.get("/demo", (req, res) => {
+  res.status(200).send("API results");
 });
 
 /**
  * @swagger
- * /customers:
+ * /demo:
  *    put:
- *      description: Use to return all customers
+ *      description: Use to put
  *    parameters:
- *      - name: customer
+ *      - name: user
  *        in: query
- *        description: Name of our customer
+ *        description: Name of a user
  *        required: false
  *        schema:
  *          type: string
@@ -77,10 +63,23 @@ app.get("/customers", (req, res) => {
  *      '201':
  *        description: Successfully created user
  */
-app.put("/customer", (req, res) => {
-  res.status(200).send("Successfully updated customer");
-});
 
-app.listen(port, () => {
+// write to json file and close server
+const fs = require('fs');
+
+fs.writeFile("apiSpec.json", JSON.stringify(swaggerDocs, null, 4), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+
+// close server so the build wont hang
+    server.close(() => {
+      console.log(`Server closed`);
+    });
+}); 
+
+
+var server = app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
