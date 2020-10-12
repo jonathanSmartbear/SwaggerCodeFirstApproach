6da7cef23a619e6ccdf9046ed3e1c6dc542a15de
jonathanSmartbear <56030954+jonathanSmartbear@users.noreply.github.com>
2020-10-12 16:57:48 -0400

    Update stage2.js

--- stage2.js
+++ stage2.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const swaggerDocs = require("swaggerDocs");
 
 // const API_KEY = process.env.SWAGGERHUB_KEY;
-const API_KEY = fa8f415b-bd39-450a-bbf0-9033b48b8029;
+const API_KEY = "fa8f415b-bd39-450a-bbf0-9033b48b8029";
 
 const openAPIPath = "./docs/apiSpec.json";
 const owner = "JHDemoProjects";
@@ -15,35 +15,35 @@ const apiName = "DemoExample";
 const IMPORT_URL = `https://api.swaggerhub.com/apis/${owner}/${apiName}?isPrivate=true&version=1.0.0&projectName=${projectFolder}`;
 
 // write to json file
-fs.writeFile(openAPIPath, JSON.stringify(swaggerDocs, null, 4), function(err) {
-    if(err) {
+fs.writeFile(openAPIPath, JSON.stringify(swaggerDocs, null, 4), function (err) {
+    if (err) {
         return console.log(err);
     }
     console.log(`/n/n ${openAPIPath}`);
     console.log("The file was saved!");
- }); 
+});
 
-async function import() {
-  try {
-    // Import
-    console.log(`Importing API ${version}`);
-    await axios({
-      url: PUBLISH_URL,
-      method: "POST",
-      headers: {
-        authorization: API_KEY,
-        "Content-Type": "application/json",
-      },
-      data: {openAPIPath},
-    });
-
-  } catch (error) {
-    console.error(error.message);
-    process.exit(1);
-  }
-  console.log("Complete!");
-}
 
 (async function () {
-  await import();
+    await import()
+    {
+        try {
+            // Import
+            console.log(`Importing API ${version}`);
+            await axios({
+                url: PUBLISH_URL,
+                method: "POST",
+                headers: {
+                    authorization: API_KEY,
+                    "Content-Type": "application/json",
+                },
+                data: { openAPIPath },
+            });
+
+        } catch (error) {
+            console.error(error.message);
+            process.exit(1);
+        }
+        console.log("Complete!");
+    };
 })();
