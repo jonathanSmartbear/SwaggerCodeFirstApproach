6ab8ff38ba8f729a0b879f7033b5e097fd2833f2
jonathanSmartbear <56030954+jonathanSmartbear@users.noreply.github.com>
2020-10-12 16:41:10 -0400

    Update stage2.js

--- stage2.js
+++ stage2.js
@@ -10,15 +10,16 @@ const API_KEY = fa8f415b-bd39-450a-bbf0-9033b48b8029;
 const openAPIPath = "./docs/apiSpec.json";
 const owner = "JHDemoProjects";
 const projectFolder = "InternalAPIs";
-const api = "DemoExample";
+const apiName = "DemoExample";
 
-const IMPORT_URL = `https://api.swaggerhub.com/apis/${owner}/${api}?isPrivate=true&version=1.0.0&projectName=${projectFolder};
+const IMPORT_URL = `https://api.swaggerhub.com/apis/${owner}/${apiName}?isPrivate=true&version=1.0.0&projectName=${projectFolder}`;
 
 // write to json file
 fs.writeFile(openAPIPath, JSON.stringify(swaggerDocs, null, 4), function(err) {
     if(err) {
         return console.log(err);
     }
+    console.log(`/n/n ${openAPIPath}`);
     console.log("The file was saved!");
  }); 
 
