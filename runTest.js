7023b4b0b0b02b2bb276bd9ca6d302d249be997b
Jonathan Harris <Jonathan.Harris@smartbear.com>
2020-10-14 14:53:07 -0400

    push changes and initiate actions

---
+++ runTest.js
@@ -0,0 +1,28 @@
+
+
+
+///////////// POST TO testEngine ////////////////////////////////////////////////
+const owner = "JHDemoProjects";
+const projectFolder = "InternalAPIs";
+const apiName = "DemoExample";
+const API_KEY = "testengine";
+const testJob = "testJobString";
+
+const TestEngine_Url = `http://ec2-3-129-173-1.us-east-2.compute.amazonaws.com:8080/api/v1/testjobs?fetch=5`;
+const basicAuth = {
+  'username': 'jonathan',
+  'password': API_KEY
+}
+/////////////////////////////////////////////
+axios.get(TestEngine_Url,{
+  auth: basicAuth
+})
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(err => console.log(err));
+
+  console.log("Sent Test To TestEngine!");
+////////////////////////////////////////////////////////////////////////////////
+
+
