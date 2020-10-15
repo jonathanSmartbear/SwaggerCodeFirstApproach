aec2da2b040fbcbb26b1c5b7b66b2f713536f96b
Jonathan Harris <Jonathan.Harris@smartbear.com>
2020-10-15 11:30:50 -0400

    push changes and initiate actions

--- runTest.js
+++ runTest.js
@@ -1,5 +1,5 @@
 
-
+const fs = require('fs');
 
 ///////////// POST TO testEngine ////////////////////////////////////////////////
 const axios = require("axios");
@@ -8,19 +8,26 @@ const owner = "JHDemoProjects";
 const projectFolder = "InternalAPIs";
 const apiName = "DemoExample";
 const API_KEY = "testengine";
-const testJob = "testJobString";
+const xmlFile = fs.readFile("./tests/TestEngineSchemaC-readyapi-project.xml", 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+//  console.log(data)
+})
 
-const TestEngine_Url = `http://ec2-3-129-173-1.us-east-2.compute.amazonaws.com:8080/api/v1/testjobs?fetch=1`;
+const TestEngine_Url = `http://ec2-3-129-173-1.us-east-2.compute.amazonaws.com:8080/api/v1/testjobs`;
 const basicAuth = {
   'username': 'jonathan',
   'password': API_KEY
 }
 /////////////////////////////////////////////
-axios.get(TestEngine_Url, /*testJob,*/ {
-  auth: basicAuth
+axios.post(TestEngine_Url, xmlFile, {
+  'auth': basicAuth,
+  'Content-Type': 'application/xml'
 })
-  .then(function (response) {
-    console.log(response);
+  .then(function (reqwest) {
+    console.log(reqwest);
   })
   .catch(err => console.log(err));
 
